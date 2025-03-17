4d2dc9cf1edab0f144ac30204289b9909a033e76
Amr Walidi <amr.nawaf@gmail.com>
2025-03-17 15:07:10 +0300

    add answer validation for enter number question

--- app/src/main/java/com/amrwalidi/android/fancup/ui/fragment/EnterNumberFragment.kt
+++ app/src/main/java/com/amrwalidi/android/fancup/ui/fragment/EnterNumberFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.amrwalidi.android.fancup.R
@@ -40,6 +41,13 @@ class EnterNumberFragment : Fragment() {
         binding.lifecycleOwner = this
 
         binding.questionText.text = question?.text ?: ""
+
+        viewModel?.message?.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
+                Toast.makeText(requireActivity(), it, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/amrwalidi/android/fancup/viewmodel/EnterNumberViewModel.kt
+++ app/src/main/java/com/amrwalidi/android/fancup/viewmodel/EnterNumberViewModel.kt
@@ -2,12 +2,25 @@ package com.amrwalidi.android.fancup.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.amrwalidi.android.fancup.domain.Question
 
-class EnterNumberViewModel(application: Application, question: Question?) :
+class EnterNumberViewModel(application: Application, val question: Question?) :
     AndroidViewModel(application) {
+
+    val answer = MutableLiveData("")
+
+    private val _message = MutableLiveData("")
+    val message: LiveData<String>
+        get() = _message
+
+    fun submit(){
+        if (answer.value == question?.answers?.get(0)) _message.value = "CORRECT ANSWER"
+        else _message.value = "WRONG ANSWER"
+    }
 
     class Factory(val app: Application, val question: Question?) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
